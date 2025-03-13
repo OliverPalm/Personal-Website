93f5bcd5bfa10ecaaf3fea7f452f74f6f59f483f
Oliver Palm <Oliver.palm35@gmail.com>
2025-03-13 21:17:35 +0100

    aiwjd

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -5,7 +5,7 @@ import ProjectCard from "../../common/ProjectCard";
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
-      <h1 className="sectionTitle">Projects</h1>
+      <h1 className="sectionTitle">Games</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
           src={viberr}
